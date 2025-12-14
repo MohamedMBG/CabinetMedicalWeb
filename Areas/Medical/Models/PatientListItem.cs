0b59e62a4449e722e480d8eba74d0566e05a1213
BAGHDAD Mohamed <123114744+MohamedMBG@users.noreply.github.com>
2025-12-14 03:01:42 +0100

    Fix patient selection crash by sharing patient list model

---
+++ Areas/Medical/Models/PatientListItem.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace CabinetMedicalWeb.Areas.Medical.Models
+{
+    public record PatientListItem(
+        [property: JsonPropertyName("id")] int Id,
+        [property: JsonPropertyName("fullName")] string FullName,
+        [property: JsonPropertyName("telephone")] string Telephone,
+        [property: JsonPropertyName("email")] string Email,
+        [property: JsonPropertyName("dateNaissance")] string DateNaissance
+    );
+}
