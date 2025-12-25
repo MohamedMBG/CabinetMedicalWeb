e9ac21c00374eafa13cc856b1059010b66bb3bd1
MohamedMBG <mohamedbaghdad.me@gmail.com>
2025-12-25 13:53:21 +0100

    Add migration for status column in Conges table and create migrations for AddAdminFeature and AddStatutToRendezVous

--- Data/Migrations/20251225124851_AddStatutToRendezVous.cs
+++ Data/Migrations/20251225124851_AddStatutToRendezVous.cs
@@ -5,25 +5,18 @@
 namespace CabinetMedicalWeb.Data.Migrations
 {
     /// <inheritdoc />
-    public partial class AddAdminFeatures3 : Migration
+    public partial class AddStatutToRendezVous : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.AddColumn<int>(
-                name: "Status",
-                table: "Conges",
-                type: "int",
-                nullable: false,
-                defaultValue: 0);
+
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropColumn(
-                name: "Status",
-                table: "Conges");
+
         }
     }
 }
